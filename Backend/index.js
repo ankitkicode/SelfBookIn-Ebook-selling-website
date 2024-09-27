3871184bd4df59c800b99c7d4d2e17280a22f71e
ankitkicode <aj0353342@gmail.com>
2024-09-27 13:52:20 +0530

    database updated

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://selfbookin-ebook-selling-website.onrender.com',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true
     }
